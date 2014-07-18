7c47f8b50fb42930bf18f9a20cf6341879152069
Andriy Goltsev <goltsev.andriy@gmail.com>
2014-07-18 15:42:17 -0400

    initial commit

---
+++ app/src/main/java/org/securechat/andriygoltsev/securechatapp/ChatActivity.java
@@ -0,0 +1,84 @@
+package org.securechat.andriygoltsev.securechatapp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.*;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+
+@ContentView(R.layout.fragment_chat)
+public class ChatActivity extends RoboActivity {
+
+    @InjectView(R.id.sendButton)
+    private Button sendButton;
+
+    @InjectView(R.id.textView)
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_chat);
+//        if (savedInstanceState == null) {
+//            getFragmentManager().beginTransaction()
+//                    .add(R.id.container, new PlaceholderFragment())
+//                    .commit();
+//        }
+
+        sendButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Hello there");
+            }
+        });
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.chat, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+            return rootView;
+        }
+    }
+}
